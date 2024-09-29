07b413cf00280e589eb74bf3dedcf1f028474675
Moonsung Bae <qoanstjdsla@gmail.com>
2024-09-30 03:53:42 +0900

    first commit

---
+++ project 'SEXY'.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome('C:\\Users\\USER\\Desktop\\chromedriver_win32\\chromedriver.exe')
+
+browser.get('https://psbacademyedu.blackboard.com/')
+
+browser.implicitly_wait(3)
+
+agree = browser.find_element_by_class_name('button-1')
+agree.click()
+
+
+
+id = browser.find_element_by_id('user_id')
+id.send_keys('086la980@student.psb.edu.sg')
+
+id = browser.find_element_by_id('password')
+id.send_keys('Qoanstjd99!')
+
+sign_up = browser.find_element_by_class_name('button.expand.inverse.outline')
+sign_up.click()
+
+browser.implicitly_wait(5)
+
+browser.get('https://psbacademyedu.blackboard.com/ultra/course')
+
+#courses = browser.find_element_by_class_name('base-navigation-button-content themed-background-primary-alt-fill-only theme-border-left-active')
+#courses.click()
+browser.implicitly_wait(3)
+
+crypto = browser.find_element_by_id('course-link-_2893_1')
+crypto.click()
+
+browser.implicitly_wait(3)
+
+join1 = browser.find_element_by_id('sessions-list-dropdown')
+join1.click()
+
+joinfianl = browser.find_element_by_id('sessions-list')
+joinfianl.click()
